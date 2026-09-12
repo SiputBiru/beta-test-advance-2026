a67f566b56533570b0cb56bb5bca294196f113a2
SiputBiru <hillsforrest03@gmail.com>
2026-09-12 09:52:20 +0700

    just trying fomatting stuff

--- day2/Hands-On/PageNode.java
+++ day2/Hands-On/PageNode.java
@@ -2,37 +2,37 @@
  * A node in a doubly-linked list representing one page of a note.
  */
 public class PageNode {
-    private final int pageNumber;
-    private final String content;
-    private PageNode prev;
-    private PageNode next;
+  private final int pageNumber;
+  private final String content;
+  private PageNode prev;
+  private PageNode next;
 
-    public PageNode(int pageNumber, String content) {
-        this.pageNumber = pageNumber;
-        this.content = content;
-    }
+  public PageNode(int pageNumber, String content) {
+    this.pageNumber = pageNumber;
+    this.content = content;
+  }
 
-    public int getPageNumber() {
-        return pageNumber;
-    }
+  public int getPageNumber() {
+    return pageNumber;
+  }
 
-    public String getContent() {
-        return content;
-    }
+  public String getContent() {
+    return content;
+  }
 
-    public PageNode getPrev() {
-        return prev;
-    }
+  public PageNode getPrev() {
+    return prev;
+  }
 
-    public void setPrev(PageNode prev) {
-        this.prev = prev;
-    }
+  public void setPrev(PageNode prev) {
+    this.prev = prev;
+  }
 
-    public PageNode getNext() {
-        return next;
-    }
+  public PageNode getNext() {
+    return next;
+  }
 
-    public void setNext(PageNode next) {
-        this.next = next;
-    }
+  public void setNext(PageNode next) {
+    this.next = next;
+  }
 }
